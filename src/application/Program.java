86a0c2987dd57f7607bf5de08569ea8dd5726f6f
Yuri Carias <yuristricken7@gmail.com>
2024-01-15 20:54:10 -0300

    SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +14,8 @@ public class Program {
 		
 		
 		Seller seller = new Seller(21, "Bob", "Bob@gmail.com", new Date(), 3000.0, obj);
+		
+		Sellerdao sellerdao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 
