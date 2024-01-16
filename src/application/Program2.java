fb75ad06f8e905e90632c3fba02dd10b69c0276a
Yuri Carias <yuristricken7@gmail.com>
2024-01-16 12:22:46 -0300

    Implementação da class DepartmentDaoJDBC, DaoFactory e insert

---
+++ src/application/Program2.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("\n=== TEST 4: seller insert ====");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		
+		
+		sc.close();
+
+	}
+
+}
