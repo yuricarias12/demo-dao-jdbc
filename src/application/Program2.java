61a65785c531d864b64ea305abf6994ee272c90a
Yuri Carias <yuristricken7@gmail.com>
2024-01-16 12:52:43 -0300

    Implementação department findById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,12 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
+
 
 public class Program2 {
 
@@ -14,10 +16,39 @@ public class Program2 {
 		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
-		System.out.println("\n=== TEST 4: seller insert ====");
+		System.out.println("=== TEST 1: department findById ====");
+		Department department = departmentDao.findById(5);
+		System.out.println(department);
+		
+		/*System.out.println("\n=== TEST 2: seller findByDepartment ====");
+		Department department = new Department(2, null);
+		List<Department> list = departmentDao.findByDepartment(department);
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== TEST 3: seller findAll ====");
+		list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("\n=== TEST 4: department insert ====");
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New id = " + newDepartment.getId());
+		
+		System.out.println("\n=== TEST 5: department update ====");
+		department = departmentDao.findById(5);
+		department.setName("Martha Waine");
+		departmentDao.update(department);
+		System.out.println("Update completed");
+		
+		System.out.println("\n=== TEST 6: seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete complete");*/
 		
 		
 		
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 import db.DB;
@@ -12,47 +11,18 @@ import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+
 public class DepartmentDaoJDBC implements DepartmentDao {
 	
 	private Connection conn;
-	
-	public DepartmentDaoJDBC(Connection conn) {
-		this.conn = conn;
-	}
+		
+		public DepartmentDaoJDBC(Connection conn) {
+			this.conn = conn;
+		}
 
 	@Override
 	public void insert(Department obj) {
-		PreparedStatement st = null;
-		try {
-			st = conn.prepareStatement(
-					"INSERT INTO department "
-					+ "(Name) "
-					+ "VALUES "
-					+ "(?)", 
-					Statement.RETURN_GENERATED_KEYS);
-			
-			st.setString(1, obj.getName());
-			
-			int rowsAffected = st.executeUpdate();
-			
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				if (rs.next()) {
-					int id = rs.getInt(1);
-					obj.setId(id);
-				}
-				DB.closeResultSet(null);
-			}
-			else {
-				throw new DbException("Unexpected error! No rows affected!");
-			}		
-		}
-		catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		}
-		finally {
-			DB.closeStatement(st);
-		}
+		// TODO Auto-generated method stub
 		
 	}
 
@@ -70,8 +40,37 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public Department findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT department .*,department.Name as DepName "
+					+ "FROM department "
+					+ "WHERE Id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				
+				return dep;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep = new Department();
+		dep.setId(rs.getInt("Id"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
